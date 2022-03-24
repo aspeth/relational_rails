8e4bf7ffd5ba9d43949b8cfcb29cc64e87f06f95
Andrew Speth <speth.andrew@yahoo.com>
2022-03-23 20:28:53 -0600

    feat: companies#index shows all companies

--- spec/features/companies_index_spec.rb
+++ spec/features/companies_index_spec.rb
@@ -3,10 +3,14 @@ require 'rails_helper'
 RSpec.describe 'companies#index' do
   it 'shows all companies' do
     company_1 = Company.create!(name: "Never Summer", based_in_co: true, year_founded: 1991)
+    company_2 = Company.create!(name: "Burton", based_in_co: false, year_founded: 1977)
 
     visit '/companies'
     expect(page).to have_content(company_1.name)
     expect(page).to have_content(company_1.based_in_co)
     expect(page).to have_content(company_1.year_founded)
+    expect(page).to have_content(company_2.name)
+    expect(page).to have_content(company_2.based_in_co)
+    expect(page).to have_content(company_2.year_founded)
   end
 end
