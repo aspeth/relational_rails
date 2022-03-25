27c5e0f0a314e3429ca79790995151b3d3e69286
Andrew Speth <speth.andrew@yahoo.com>
2022-03-24 20:25:50 -0600

    test: companies/:id shows the count of snowboards per company

--- spec/features/companies/show_spec.rb
+++ spec/features/companies/show_spec.rb
@@ -14,4 +14,16 @@ RSpec.describe 'company/:id' do
     expect(page).to_not have_content(burton.based_in_co)
     expect(page).to_not have_content(burton.year_founded)
   end
+
+  it 'shows the count of snowboards per company' do
+    never_summer = Company.create!(name: "Never Summer", based_in_co: true, year_founded: 1991)
+    burton = Company.create!(name: "Burton", based_in_co: false, year_founded: 1977)
+
+    visit "/companies/#{never_summer.id}"
+    expect(page).to have_content(never_summer.snowboard_count)
+    expect(page).to_not have_content(burton.snowboard_count)
+    visit "/companies/#{burton.id}"
+    expect(page).to have_content(burton.snowboard_count)
+    expect(page).to_not have_content(never_summer.snowboard_count)
+  end
 end
