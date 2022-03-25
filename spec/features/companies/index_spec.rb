75de608372f31e7739aac8e517fe3cfb17c7a018
Andrew Speth <speth.andrew@yahoo.com>
2022-03-24 20:10:36 -0600

    test: companies sort by most recently created

--- spec/features/companies/index_spec.rb
+++ spec/features/companies/index_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'companies#index' do
+
+  before(:each) do
+
+  end
+
   it 'shows all companies' do
     company_1 = Company.create!(name: "Never Summer", based_in_co: true, year_founded: 1991)
     company_2 = Company.create!(name: "Burton", based_in_co: false, year_founded: 1977)
@@ -8,5 +13,13 @@ RSpec.describe 'companies#index' do
     visit '/companies'
     expect(page).to have_content(company_1.name)
     expect(page).to have_content(company_2.name)
+  end
+
+  it 'sorts by most recently created' do
+    company_1 = Company.create!(name: "Never Summer", based_in_co: true, year_founded: 1991)
+    company_2 = Company.create!(name: "Burton", based_in_co: false, year_founded: 1977)
+
+    visit '/companies'
+    expect(company_2.name).to appear_before(company_1.name)
   end
 end
