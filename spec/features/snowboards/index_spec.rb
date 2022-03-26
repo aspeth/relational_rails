ff98712f3e4928c228d59f8d94df811a5d800dc6
Andrew Speth <speth.andrew@yahoo.com>
2022-03-26 10:37:23 -0600

    test: snowboards#index displays link to company index

--- spec/features/snowboards/index_spec.rb
+++ spec/features/snowboards/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'snowboards#index' do
   it 'shows all snowboards' do
     never_summer = Company.create!(name: "Never Summer", based_in_co: true, year_founded: 1991)
     burton = Company.create!(name: "Burton", based_in_co: false, year_founded: 1977)
-    
+
     insta_gator = Snowboard.create!(name: "InstaGator", powder_board: true, length: 156, company_id: never_summer.id)
     process = Snowboard.create!(name: "Process Flying V", powder_board: false, length: 157, company_id: burton.id)
 
@@ -15,5 +15,21 @@ RSpec.describe 'snowboards#index' do
     expect(page).to have_content(process.name)
     expect(page).to have_content(process.powder_board)
     expect(page).to have_content(process.length)
+  end
+
+  it 'displays a link to the companies index' do
+    visit '/snowboards'
+    expect(page).to have_link('Company Index')
+    click_link('Company Index')
+
+    expect(page).to have_content(@never_summer.name)
+    expect(page).to have_content(@burton.name)
+
+    visit '/snowboards'
+    expect(page).to have_link('Company Index')
+    click_link('Company Index')
+
+    expect(page).to have_content(@never_summer.name)
+    expect(page).to have_content(@burton.name)
   end
 end
